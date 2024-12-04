37872300cc8ef79b036d044898cab688ff21c952
Q-tip <newton21989+aoc2024@gmail.com>
2024-12-03 22:39:47 -0500

    Day 1 part 2 complete

--- day01/day1.php
+++ day01/day1.php
@@ -6,9 +6,10 @@ $file = file_get_contents("day1.txt", true);
 if(!$file)
   die("Colud not open input file.");
 
-$lines = explode("\n", $file);
+$lines = explode("\n", trim($file));
+
 foreach($lines as $line) {
-  $l = explode("   ", $line);
+  $l = explode("   ", trim($line));
   //echo "$line = " . $l[0] . ", " . $l[1] . "\n";
   if(!is_null($l[0]) && !is_null($l[1])) {
     array_push($l1, $l[0]);
@@ -18,6 +19,22 @@ foreach($lines as $line) {
 
 //var_dump($l1, $l2);
 
+//part 2
+$freqs = [];
+foreach($l2 as $l) {
+  if(!key_exists((string)$l, $freqs)) {
+    $freqs[$l] = 0;
+  }
+  $freqs[(string)$l]++;
+}
+
+$ans2 = 0;
+foreach($l1 as $l) {
+  $freq = $freqs[(string)$l] ?? 0;
+  $ans2 += $l * $freq;
+}
+
+//part 1
 array_multisort($l1);
 array_multisort($l2);
 
@@ -26,4 +43,5 @@ foreach($l1 as $k=>$l) {
   $ans += abs($l - $l2[$k]);
 }
 
-echo "Day 1 part 1 answer: $ans";
+echo "Day 1 part 1 answer: $ans\n";
+echo "Day 1 part 2 answer: $ans2\n";
