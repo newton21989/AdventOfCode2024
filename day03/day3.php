dbfbbd1a17c1e7ed964d8a319476e525396955bd
Q-tip <newton21989+aoc2024@gmail.com>
2024-12-05 20:05:28 -0600

    Day 3 part 2 complete

--- day03/day3.php
+++ day03/day3.php
@@ -5,14 +5,30 @@ if(!$file)
   die("Colud not open input file.");
 
 $m = [];
-preg_match_all('/mul\((\d+),(\d+)\)/', trim($file), $m);
+preg_match_all('/do\(\)|don\'t\(\)|mul\((\d+),(\d+)\)/', trim($file), $m);
 
 //var_dump($m);
 
 $out1 = 0;
 foreach($m[0] as $k=>$n) {
+  //ignore part 2 nonsense for now
+  if($n == "do()" || $n == "don't()") {
+    continue; }
   //echo $m[1][$k] . " * " . $m[2][$k] . "\n";
   $out1 += $m[1][$k] * $m[2][$k];
 }
 
-echo "Day 3 part 1 answer: $out1";
+$out2 = 0;
+$do = true;
+foreach($m[0] as $k=>$n) {
+  if($n == "do()") {
+    $do = true; }
+  elseif($n == "don't()") {
+    $do = false; }
+  elseif(str_starts_with($n, "mul(") && $do) {
+    $out2 += $m[1][$k] * $m[2][$k];
+  }
+}
+
+echo "Day 3 part 1 answer: $out1\n";
+echo "Day 3 part 2 answer: $out2\n";
